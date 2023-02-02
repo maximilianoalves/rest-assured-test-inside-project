23d85eed339ea4124664e8cb1cc7a8fc8ec72aa6
Maximiliano Alves <maximilianodacruz@gmail.com>
2023-02-02 11:33:07 -0300

    add tests

--- src/test/java/br/com/maximilianoalves/tests/tutorials/GetTutorials.java
+++ src/test/java/br/com/maximilianoalves/tests/tutorials/GetTutorials.java
@@ -1,17 +1,23 @@
-package br.com.maximilianoalves.tests;
+package br.com.maximilianoalves.tests.tutorials;
 
 import br.com.maximilianoalves.BaseTests;
+import br.com.maximilianoalves.tests.auth.PostAuth;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class GetTutorials extends BaseTests {
 
+    PostAuth postAuth = new PostAuth();
+
     @Test
     public void shouldBeReturnedAllTutorials() {
+        String bearerToken = postAuth.getBearerToken();
+
         given()
+                .header("Authorization", bearerToken)
             .when()
-                .get("api/tutorials")
+                .get("api/v1/tutorials")
             .then()
                 .statusCode(200);
     }
